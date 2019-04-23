9194da7e9dc1b40d9d03aecbda9442bc8b8dac37
Raghav Kumar Jha <raghav.jha@Pratian.com>
2019-04-23 18:43:37 +0530

    Groovy file modified

--- jobDSL.groovy
+++ jobDSL.groovy
@@ -5,4 +5,15 @@ scm {
     triggers {
         scm('H/15 * * * *')
     }
+	  steps {
+        msBuild {
+            msBuildInstallation('MSBuild 1.8')
+            buildFile('dir1/build.proj')
+            args('check')
+            args('another')
+            passBuildVariables()
+            continueOnBuildFailure()
+            unstableIfWarnings()
+        }
+    }
 }
